3086736c010d372088aac50580681b9eeb052841
Garon <garoncdavis@gmail.com>
2017-05-14 00:31:29 -0400

    sample album app completed

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
-import Kinvey from 'kinvey-html5-sdk';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
   state = { albums: [] };
 
   componentWillMount() {
-    Kinvey.initialize({
-      appKey: 'kid_H1Fi5P0kb',
-      appSecret: '778f370802c94ce884a589b5f279142b'
-    });
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
       .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} data={album} />
+    );
   }
 
   render() {
     console.log(this.state);
     return (
-      <View>
-        <Text>Album List!!!</Text>
-      </View>
+      <ScrollView>
+        {this.renderAlbums()}
+      </ScrollView>
     );
   }
 }
